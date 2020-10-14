435778bb967de1f377e8dd2ec8b0dabb872ca8a6
shubhamverma845 <shubhamverma845@gmail.com>
2020-10-14 15:41:40 +0530

    Added problem7.java

---
+++ problem7.java
@@ -0,0 +1,50 @@
+//10001st prime
+
+import java.lang.Math;
+
+
+class problem7{
+
+	static boolean isPrime(int n){
+
+		if(n == 2){
+			return true;
+		}
+
+		for(int i = 2;i <= Math.sqrt(n) ;i++){
+			
+			if(n % i == 0){
+				return false;
+			} else{
+				continue;
+			}
+		}
+
+		return true;
+	}
+
+	public static void main(String[] args) {
+
+
+		// long start = System.nanoTime();
+
+		int count = 1;
+		int n = 2;
+
+		while(count < 10001){
+			n++;
+
+			if(isPrime(n) == true){
+				count++;
+			}
+
+		}
+
+		System.out.println(n);
+
+		// long time = System.nanoTime() - start;
+
+		// System.out.println(time);
+		
+	}
+}
